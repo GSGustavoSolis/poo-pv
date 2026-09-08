f3bc7f4ee3dca4bd38e6a73342365db4e3bb21cc
GSGustavoSolis <gustavo.solis@estudante.iftm.edu.br>
2026-09-08 20:58:58 -0300

    ola

---
+++ poo/CadastroAnimaisExercicio/Entidades/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroAnimaisExercicio.Entidades
+{
+    public class Animal
+    {
+        public string Nome;
+        public string Especie;
+        public int Idade;
+
+        public Animal(string nome, string especie, int idade)
+        {
+            Nome = nome;
+            Especie = especie;
+            Idade = idade;
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine("teste");
+            Console.WriteLine($"Espécie: {Especie}");
+            Console.WriteLine($"Idade: {Idade} anos");
+        }
+    }
+}
